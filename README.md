d06ce27feca16a841007bf203f4c714b07891b22
James Richey <49895469+j-richey@users.noreply.github.com>
2019-12-14 13:51:13 -0800

    Add library usage example to README
    The usage example shows how to get started using the library. It is
    important that the example work as expected so the doc-comment crate
    is used to hook the example into Rust's document test system.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # open_ttt_lib
+
+[![Crates.io](https://img.shields.io/crates/v/open_ttt_lib.svg)](https://crates.io/crates/open_ttt_lib)
+[![Documentation](https://docs.rs/open_ttt_lib/badge.svg)](https://docs.rs/open_ttt_lib)
+[![License](https://img.shields.io/crates/l/open_ttt_lib.svg)](https://github.com/j-richey/open_ttt_lib/blob/master/LICENSE.txt)
 [![Build Status](https://travis-ci.com/j-richey/open_ttt_lib.svg?branch=master)](https://travis-ci.com/j-richey/open_ttt_lib)
 [![Coverage Status](https://coveralls.io/repos/github/j-richey/open_ttt_lib/badge.svg?branch=master)](https://coveralls.io/github/j-richey/open_ttt_lib?branch=master)
 
@@ -15,6 +19,58 @@ Add this to your `Cargo.toml`:
 [dependencies]
 open_ttt_lib = "0.1"
 ```
+
+Below is an example of using this library. Two AI opponents to play a game of
+Tic Tac Toe until one is victorious.
+
+```rust
+use open_ttt_lib::{ai, game};
+ 
+fn main() -> Result<(), Box<game::Error>> {
+    // Create a game and two AI opponents to play the game.
+    let mut game = game::Game::new();
+ 
+    // Rando picks random positions.
+    let rando = ai::Opponent::new(1.0);
+    // The flawless opponent cannot loose: it fully evaluates every possible
+    // move and countermove to pick the best position.
+    let flawless_ai = ai::Opponent::new(0.0);
+ 
+    // Have the opponents take turns making moves until the game is over.
+    loop {
+        match game.state() {
+            game::State::PlayerXMove => {
+                println!("Rando playing as X turn:");
+                game.do_move(rando.get_move(&game).unwrap())?;
+            }
+            game::State::PlayerOMove => {
+                println!("Flawless AI playing as O turn:");
+                game.do_move(flawless_ai.get_move(&game).unwrap())?;
+            }
+            game::State::PlayerXWin(_) => {
+                println!("Game Over: Rando playing as X wins!");
+                break;
+            }
+            game::State::PlayerOWin(_) => {
+                println!("Game Over: Flawless AI playing as O wins!");
+                break;
+            }
+            game::State::CatsGame => {
+                println!("Game Over: cat's game.");
+                break;
+            }
+        };
+ 
+        // Print the game's the board.
+        println!("{}", game.board());
+    }
+ 
+    Ok(())
+}
+```
+
+See the [documentation](https://docs.rs/open_ttt_lib/) for additional examples 
+and details on using the library.
 
 
 ## Feature Requests
@@ -35,9 +91,9 @@ If you find an issue please include the following information in your report:
 
 
 ## Contributing
-Contributions are welcome! See [CONTRIBUTING.md](CONTRIBUTING.md) for details on how
-to contribute to this project.
+Contributions are welcome! See [CONTRIBUTING.md](https://github.com/j-richey/open_ttt_lib/blob/master/CONTRIBUTING.md) 
+for details on how to contribute to this project.
 
 
 ## License
-The library is licensed under the [MIT license](LICENSE.txt).
+The library is licensed under the [MIT license](https://github.com/j-richey/open_ttt_lib/blob/master/LICENSE.txt).
--- src/lib.rs
+++ src/lib.rs
@@ -4,54 +4,58 @@
 //! The example below creates a Tic Tac Toe game and two AI opponents to play
 //! the game.
 //! ```
-//! # use open_ttt_lib::game;
-//! #
-//! # fn main() -> Result<(), Box<game::Error>> {
 //! use open_ttt_lib::{ai, game};
-//!
-//! // Create a game and two AI opponents to play the game.
-//! let mut game = game::Game::new();
-//!
-//! // Rando picks random positions.
-//! let rando = ai::Opponent::new(1.0);
-//! // The flawless opponent cannot loose: it fully evaluates every possible
-//! // move and countermove in the game.
-//! let flawless_ai = ai::Opponent::new(0.0);
-//!
-//! // Have the opponents take turns making moves until the game is over.
-//! loop {
-//!     match game.state() {
-//!         game::State::PlayerXMove => {
-//!             println!("Rando playing as X turn:");
-//!             game.do_move(rando.get_move(&game).unwrap())?;
-//!         }
-//!         game::State::PlayerOMove => {
-//!             println!("Flawless AI playing as O turn:");
-//!             game.do_move(flawless_ai.get_move(&game).unwrap())?;
-//!         }
-//!         game::State::PlayerXWin(_) => {
-//!             println!("Game Over: Rando playing as X wins!");
-//!             break;
-//!         }
-//!         game::State::PlayerOWin(_) => {
-//!             println!("Game Over: Flawless AI playing as O wins!");
-//!             break;
-//!         }
-//!         game::State::CatsGame => {
-//!             println!("Game Over: cat's game.");
-//!             break;
-//!         }
-//!     };
-//!
-//!     // Print the game's the board.
-//!     println!("{}", game.board());
+//! 
+//! fn main() -> Result<(), Box<game::Error>> {
+//!     // Create a game and two AI opponents to play the game.
+//!     let mut game = game::Game::new();
+//! 
+//!     // Rando picks random positions.
+//!     let rando = ai::Opponent::new(1.0);
+//!     // The flawless opponent cannot loose: it fully evaluates every possible
+//!     // move and countermove to pick the best position.
+//!     let flawless_ai = ai::Opponent::new(0.0);
+//! 
+//!     // Have the opponents take turns making moves until the game is over.
+//!     loop {
+//!         match game.state() {
+//!             game::State::PlayerXMove => {
+//!                 println!("Rando playing as X turn:");
+//!                 game.do_move(rando.get_move(&game).unwrap())?;
+//!             }
+//!             game::State::PlayerOMove => {
+//!                 println!("Flawless AI playing as O turn:");
+//!                 game.do_move(flawless_ai.get_move(&game).unwrap())?;
+//!             }
+//!             game::State::PlayerXWin(_) => {
+//!                 println!("Game Over: Rando playing as X wins!");
+//!                 break;
+//!             }
+//!             game::State::PlayerOWin(_) => {
+//!                 println!("Game Over: Flawless AI playing as O wins!");
+//!                 break;
+//!             }
+//!             game::State::CatsGame => {
+//!                 println!("Game Over: cat's game.");
+//!                 break;
+//!             }
+//!         };
+//! 
+//!         // Print the game's the board.
+//!         println!("{}", game.board());
+//!     }
+//! 
+//!     Ok(())
 //! }
-//! #   Ok(())
-//! # }
 //! ```
 
 #![doc(html_root_url = "https://docs.rs/open_ttt_lib/0.1.0")]
 
+
 pub mod ai;
 pub mod board;
 pub mod game;
+
+// Ensure the examples in the README file also work as expected.
+extern crate doc_comment;
+doc_comment::doctest!("../README.md", examples_in_readme);
